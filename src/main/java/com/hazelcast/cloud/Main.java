f9916982fb7221c9188c72fc5b430e6b1f6f7a92
enesakar <enesakar@gmail.com>
2018-10-29 00:39:55 +0300

    initial commit

---
+++ src/main/java/com/hazelcast/cloud/Main.java
@@ -0,0 +1,27 @@
+package com.hazelcast.cloud;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.spi.impl.discovery.HazelcastCloudDiscovery;
+import com.hazelcast.client.spi.properties.ClientProperty;
+import com.hazelcast.config.GroupConfig;
+import com.hazelcast.config.SSLConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ClientConfig config = new ClientConfig();
+        config.setGroupConfig(new GroupConfig("YOUR_CLUSTER_NAME", "YOUR_CLUSTER_PASSWORD"));
+        config.setProperty(ClientProperty.HAZELCAST_CLOUD_DISCOVERY_TOKEN.getName(), "YOUR_CLUSTER_DISCOVERY_TOKEN");
+        config.getNetworkConfig().setSSLConfig(new SSLConfig().setEnabled(true));
+        HazelcastInstance client = HazelcastClient.newHazelcastClient(config);
+        IMap<String, String> map = client.getMap("theGenius");
+        map.put("vaccination", "Edward Jenner");
+        map.put("alternating current motor", "Nikola Tesla");
+        map.put("polonium and radium", "Marie Curie");
+        System.out.println("You have "+ map.size() +" people in your genius map.");
+    }
+
+}
