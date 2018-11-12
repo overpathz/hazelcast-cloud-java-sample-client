25faa4a5729bd5f8189a1e86e537e3e804dba97f
enesakar <enesakar@gmail.com>
2018-11-12 15:19:26 -0800

    changes

--- src/main/java/com/hazelcast/cloud/Main.java
+++ src/main/java/com/hazelcast/cloud/Main.java
@@ -13,16 +13,13 @@ public class Main {
 
     public static void main(String[] args) {
         ClientConfig config = new ClientConfig();
-        config.setProperty("hazelcast.client.statistics.enabled","true");
         config.setGroupConfig(new GroupConfig("YOUR_CLUSTER_NAME", "YOUR_CLUSTER_PASSWORD"));
+        config.setProperty("hazelcast.client.statistics.enabled","true");
         config.setProperty(ClientProperty.HAZELCAST_CLOUD_DISCOVERY_TOKEN.getName(), "YOUR_CLUSTER_DISCOVERY_TOKEN");
-        config.getNetworkConfig().setSSLConfig(new SSLConfig().setEnabled(true));
         HazelcastInstance client = HazelcastClient.newHazelcastClient(config);
-        IMap<String, String> map = client.getMap("theGenius");
-        map.put("vaccination", "Edward Jenner");
-        map.put("alternating current motor", "Nikola Tesla");
-        map.put("polonium and radium", "Marie Curie");
-        System.out.println("You have "+ map.size() +" people in your genius map.");
+        IMap<String, String> map = client.getMap("map");
+        map.put("key", "value");
+        System.out.println("Value:"+ map.get("key"));
     }
 
 }
