bdcda7eec9fca0abfac84b691b4ebce8f966bea3
Huseyin BABAL <huseyinbabal88@gmail.com>
2018-12-13 02:46:05 +0300

    Refactor

--- src/main/java/com/hazelcast/cloud/ClientWithSsl.java
+++ src/main/java/com/hazelcast/cloud/ClientWithSsl.java
@@ -17,14 +17,15 @@ public class ClientWithSsl {
       ClassLoader classLoader = ClientWithSsl.class.getClassLoader();
       Properties props = new Properties();
       props.setProperty("javax.net.ssl.keyStore", classLoader.getResource("client.keystore").getPath());
-      props.setProperty("javax.net.ssl.keyStorePassword", "YOUR_KEYSTORE_PASSWORD");
+      props.setProperty("javax.net.ssl.keyStorePassword", "YOUR_SSL_PASSWORD");
       props.setProperty("javax.net.ssl.trustStore", classLoader.getResource("client.truststore").getPath());
-      props.setProperty("javax.net.ssl.trustStorePassword", "YOUR_TRUSTSTORE_PASSWORD");
+      props.setProperty("javax.net.ssl.trustStorePassword", "YOUR_SSL_PASSWORD");
       ClientConfig config = new ClientConfig();
       config.getNetworkConfig().setSSLConfig(new SSLConfig().setEnabled(true).setProperties(props));
       config.getGroupConfig().setName("YOUR_CLUSTER_NAME").setPassword("YOUR_CLUSTER_PASSWORD");
       config.setProperty("hazelcast.client.statistics.enabled","true");
       config.setProperty(ClientProperty.HAZELCAST_CLOUD_DISCOVERY_TOKEN.getName(), "YOUR_CLUSTER_DISCOVERY_TOKEN");
+      config.setProperty(HazelcastCloudDiscovery.CLOUD_URL_BASE_PROPERTY.getName(), "YOUR_DISCOVERY_URL");
       HazelcastInstance client = HazelcastClient.newHazelcastClient(config);
       IMap<String, String> map = client.getMap("map");
       map.put("key", "value");
