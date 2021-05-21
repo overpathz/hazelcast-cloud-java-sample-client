daa2b2308faa0bfc143f6ad324339812a3afa8a4
Sergey Dreger <sergey.dreger@hazelcast.com>
2021-05-21 14:54:38 +0300

    HZC-489 - Remove the internal class 'HazelcastCloudDiscovery' import
    (cherry picked from commit 477aac06fea8c49b50ad3d25b6ac3e12fe0f112f)

--- src/main/java/com/hazelcast/cloud/ClientWithSsl.java
+++ src/main/java/com/hazelcast/cloud/ClientWithSsl.java
@@ -2,7 +2,6 @@ package com.hazelcast.cloud;
 
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.client.spi.impl.discovery.HazelcastCloudDiscovery;
 import com.hazelcast.client.spi.properties.ClientProperty;
 import com.hazelcast.config.GroupConfig;
 import com.hazelcast.config.SSLConfig;
@@ -20,6 +19,8 @@ import java.util.Random;
  */
 public class ClientWithSsl {
 
+    private static final String CLOUD_URL_BASE_PROPERTY = "hazelcast.client.cloud.url";
+
     public static void main(String[] args) throws Exception {
         ClassLoader classLoader = ClientWithSsl.class.getClassLoader();
         Properties props = new Properties();
@@ -32,7 +33,7 @@ public class ClientWithSsl {
         config.setGroupConfig(new GroupConfig("YOUR_CLUSTER_NAME", "YOUR_CLUSTER_PASSWORD"));
         config.setProperty("hazelcast.client.statistics.enabled", "true");
         config.setProperty(ClientProperty.HAZELCAST_CLOUD_DISCOVERY_TOKEN.getName(), "YOUR_CLUSTER_DISCOVERY_TOKEN");
-        config.setProperty(HazelcastCloudDiscovery.CLOUD_URL_BASE_PROPERTY.getName(), "YOUR_DISCOVERY_URL");
+        config.setProperty(CLOUD_URL_BASE_PROPERTY, "YOUR_DISCOVERY_URL");
 
         HazelcastInstance client = HazelcastClient.newHazelcastClient(config);
 
